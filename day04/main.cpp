8efab0dec5f656ecab382a7bea6839df72850c9e
DrinoSan <s.becirbegovic@gmail.com>
2024-12-04 22:00:01 +0100

    DAY 4 part 2

--- day04/main.cpp
+++ day04/main.cpp
@@ -17,14 +17,275 @@ std::vector<std::string> text;
 // .
 // .
 // y
+// PART 1
+// bool checkN( int32_t x, int32_t y )
+//{
+//   // Because we only check north direction we dont care for chexks in x dir
+//
+//   // First check if we have even enough space to check upwards
+//   if( ( y - 3 ) < 0 )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( text[y-1][x] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-2][x] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-3][x] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " N x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y-1][x] << text[y-2][x] << text[y-3][x] <<
+//   '\n'; std::cout << "+++++++++++++++++++++++++++++++\n";
+//
+//   return true;
+//}
+//
+// bool checkNE( int32_t x, int32_t y )
+//{
+//   if( ( y - 3 ) < 0 )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( ( x + 3 ) >= text[y].size() )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-1][x+1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-2][x+2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-3][x+3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " NE x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y-1][x+1] << text[y-2][x+2] <<
+//   text[y-3][x+3] << '\n'; std::cout << "+++++++++++++++++++++++++++++++\n";
+//   return true;
+//}
+//
+// bool checkE( int32_t x, int32_t y )
+//{
+//   if( ( x + 3 ) >= text[y].size() )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x+1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x+2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x+3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " E x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y][x+1] << text[y][x+2] << text[y][x+3] <<
+//   '\n'; std::cout << "+++++++++++++++++++++++++++++++\n"; return true;
+//}
+//
+// bool checkSE( int32_t x, int32_t y )
+//{
+//   if( ( y + 3 ) >= text.size() )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( ( x + 3 ) >= text[y].size() )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+1][x+1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+2][x+2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+3][x+3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " SE x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y+1][x+1] << text[y+2][x+2] <<
+//   text[y+3][x+3] << '\n'; std::cout << "+++++++++++++++++++++++++++++++\n";
+//   return true;
+//}
+//
+// bool checkS( int32_t x, int32_t y )
+//{
+//   if( ( y + 3 ) >= text.size() )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( text[y+1][x] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+2][x] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+3][x] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " S x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y+1][x] << text[y+2][x] << text[y+3][x] <<
+//   '\n'; std::cout << "+++++++++++++++++++++++++++++++\n"; return true;
+//}
+//
+// bool checkSW( int32_t x, int32_t y )
+//{
+//   if( ( y + 3 ) >= text.size() )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( ( x - 3 ) < 0 )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+1][x-1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+2][x-2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y+3][x-3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " SW x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y+1][x-1] << text[y+2][x-2] <<
+//   text[y+3][x-3] << '\n'; std::cout << "+++++++++++++++++++++++++++++++\n";
+//   return true;
+//}
+//
+// bool checkW( int32_t x, int32_t y )
+//{
+//   if( ( x - 3 ) < 0 )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x-1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x-2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y][x-3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " W x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y][x-1] << text[y][x-2] << text[y][x-3] <<
+//   '\n'; std::cout << "+++++++++++++++++++++++++++++++\n"; return true;
+//}
+//
+//
+// bool checkNW( int32_t x, int32_t y )
+//{
+//   if( ( y - 3 ) < 0 )
+//   {
+//      // no space left
+//      return false;
+//   }
+//
+//   if( ( x - 3 ) < 0 )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-1][x-1] != 'M' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-2][x-2] != 'A' )
+//   {
+//      return false;
+//   }
+//
+//   if( text[y-3][x-3] != 'S' )
+//   {
+//      return false;
+//   }
+//
+//   std::cout << "-------------------------------\n";
+//   std::cout << " NW x: " << x <<" y: " << y << '\n';
+//   std::cout << text[y][x] << text[y-1][x-1] << text[y-2][x-2] <<
+//   text[y-3][x-3] << '\n'; std::cout << "+++++++++++++++++++++++++++++++\n";
+//   return true;
+//}
 
+//========
 
 bool checkN( int32_t x, int32_t y )
 {
    // Because we only check north direction we dont care for chexks in x dir
 
    // First check if we have even enough space to check upwards
-   if( ( y - 3 ) < 0 )
+   if ( ( y - 1 ) < 0 )
    {
       // no space left
       return false;
@@ -53,233 +314,83 @@ bool checkN( int32_t x, int32_t y )
    return true;
 }
 
-bool checkNE( int32_t x, int32_t y )
+bool checkNESW( int32_t x, int32_t y )
 {
-   if( ( y - 3 ) < 0 )
+   // CHECK NORTH EAST LIMIT
+   if ( ( y - 1 ) < 0 )
    {
       // no space left
       return false;
    }
 
-   if( ( x + 3 ) >= text[y].size() )
+   if ( ( x + 1 ) >= text[ y ].size() )
    {
       return false;
    }
 
-   if( text[y-1][x+1] != 'M' )
-   {
-      return false;
-   }
-
-   if( text[y-2][x+2] != 'A' )
-   {
-      return false;
-   }
-
-   if( text[y-3][x+3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " NE x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y-1][x+1] << text[y-2][x+2] << text[y-3][x+3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
-}
-
-bool checkE( int32_t x, int32_t y )
-{
-   if( ( x + 3 ) >= text[y].size() )
-   {
-      return false;
-   }
-
-   if( text[y][x+1] != 'M' )
-   {
-      return false;
-   }
-
-   if( text[y][x+2] != 'A' )
-   {
-      return false;
-   }
-
-   if( text[y][x+3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " E x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y][x+1] << text[y][x+2] << text[y][x+3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
-}
-
-bool checkSE( int32_t x, int32_t y )
-{
-   if( ( y + 3 ) >= text.size() )
+   // CHECK SOUTH WEST LIMIT
+   if ( ( y + 1 ) >= text.size() )
    {
       // no space left
       return false;
    }
 
-   if( ( x + 3 ) >= text[y].size() )
+   if ( ( x - 1 ) < 0 )
    {
       return false;
    }
 
-   if( text[y+1][x+1] != 'M' )
+   /// UPPER RIGHT
+   if ( text[ y - 1 ][ x + 1 ] == 'M' && text[ y + 1 ][ x - 1 ] == 'S' )
    {
-      return false;
+      return true;
    }
 
-   if( text[y+2][x+2] != 'A' )
+   if ( text[ y - 1 ][ x + 1 ] == 'S' && text[ y + 1 ][ x - 1 ] == 'M' )
    {
-      return false;
+      return true;
    }
 
-   if( text[y+3][x+3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " SE x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y+1][x+1] << text[y+2][x+2] << text[y+3][x+3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
+   return false;
 }
 
-bool checkS( int32_t x, int32_t y )
+bool checkNWSE( int32_t x, int32_t y )
 {
-   if( ( y + 3 ) >= text.size() )
+   // CHECK NW LIMITS
+   if ( ( y - 1 ) < 0 )
    {
       // no space left
       return false;
    }
 
-   if( text[y+1][x] != 'M' )
+   if ( ( x - 1 ) < 0 )
    {
       return false;
    }
 
-   if( text[y+2][x] != 'A' )
-   {
-      return false;
-   }
-
-   if( text[y+3][x] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " S x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y+1][x] << text[y+2][x] << text[y+3][x] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
-}
-
-bool checkSW( int32_t x, int32_t y )
-{
-   if( ( y + 3 ) >= text.size() )
+   // CHECK SE LIMITS
+   if ( ( y + 1 ) >= text.size() )
    {
       // no space left
       return false;
    }
 
-   if( ( x - 3 ) < 0 )
+   if ( ( x + 1 ) >= text[ y ].size() )
    {
       return false;
    }
 
-   if( text[y+1][x-1] != 'M' )
+   if ( text[ y - 1 ][ x - 1 ] == 'M' && text[ y + 1 ][ x + 1 ] == 'S' )
    {
-      return false;
+      return true;
    }
 
-   if( text[y+2][x-2] != 'A' )
+   if ( text[ y - 1 ][ x - 1 ] == 'S' && text[ y + 1 ][ x + 1 ] == 'M' )
    {
-      return false;
+      return true;
    }
 
-   if( text[y+3][x-3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " SW x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y+1][x-1] << text[y+2][x-2] << text[y+3][x-3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
-}
-
-bool checkW( int32_t x, int32_t y )
-{
-   if( ( x - 3 ) < 0 )
-   {
-      return false;
-   }
-
-   if( text[y][x-1] != 'M' )
-   {
-      return false;
-   }
-
-   if( text[y][x-2] != 'A' )
-   {
-      return false;
-   }
-
-   if( text[y][x-3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " W x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y][x-1] << text[y][x-2] << text[y][x-3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
-}
-
-
-bool checkNW( int32_t x, int32_t y )
-{
-   if( ( y - 3 ) < 0 )
-   {
-      // no space left
-      return false;
-   }
-
-   if( ( x - 3 ) < 0 )
-   {
-      return false;
-   }
-
-   if( text[y-1][x-1] != 'M' )
-   {
-      return false;
-   }
-
-   if( text[y-2][x-2] != 'A' )
-   {
-      return false;
-   }
-
-   if( text[y-3][x-3] != 'S' )
-   {
-      return false;
-   }
-
-   std::cout << "-------------------------------\n";
-   std::cout << " NW x: " << x <<" y: " << y << '\n';
-   std::cout << text[y][x] << text[y-1][x-1] << text[y-2][x-2] << text[y-3][x-3] << '\n';
-   std::cout << "+++++++++++++++++++++++++++++++\n";
-   return true;
+   return false;
 }
 
 int main()
@@ -297,47 +408,18 @@ int main()
   {
      for( int32_t x = 0; x < text[y].size(); ++x )
      {
-        if( text[y][x] != 'X' )
+        // PART 1
+        // if( text[y][x] != 'X' )
+        //{
+        //   continue;
+        //}
+
+        if ( text[ y ][ x ] != 'A' )
         {
            continue;
         }
 
-        if( checkN( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkNE( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkE( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkSE( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkS( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkSW( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkW( x, y ) )
-        {
-           ++result;
-        }
-
-        if( checkNW( x, y ) )
+        if ( checkNESW( x, y ) && checkNWSE( x, y ) )
         {
            ++result;
         }
